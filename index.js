746dc0da418c148861420b9762c10156142785c8
Brinda Bhattarai <brindaabrindaa7@gmail.com>
2025-01-22 15:34:18 +0545

    8 minutes window

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(morgan("dev"))
 
 // Rate limit
 const limiter = rateLimit({
-	windowMs: 7 * 60 * 1000, // 1 minutes
+	windowMs: 8 * 60 * 1000, // 1 minutes
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
